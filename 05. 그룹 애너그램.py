4d9498dbd6d310389f0998d6968710858b60a856
Ratatou2 <Ratatou2@github.com>
2021-03-29 14:53:38 +0900

    1. 문제

---
+++ 05. 그룹 애너그램.py
@@ -0,0 +1,12 @@
+#문자열 배열을 받아 애너그램 단위로 그룹핑하라
+'''
+[ex]
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Input: strs = [""]
+Output: [[""]]
+
+Input: strs = ["a"]
+Output: [["a"]]
+'''
