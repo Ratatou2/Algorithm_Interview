c7bf66777c6dd86026529dfed217bbe2eb27f834
Ratatou2 <Ratatou2@github.com>
2021-04-01 22:47:03 +0900

    2. 다듬기 + 이해한 내용 보충

--- 07. 두 수의 합.py
+++ 07. 두 수의 합.py
@@ -14,3 +14,17 @@ print(sorted(result_list))
 
 #print(sorted(set(b))) #set으로 하면 중복제거가 됨
 # 근데 그냥 b에 겹치는 문자가 있으면 안넣게 짜두면 됨
+
+''' <브루트 포스방식>
+- 일단 이런식으로 하면 지나치게 느려짐
+- 비효율적 풀이법의 대명사
+
+여기서 조금 더 우아하게 짜보자면
+for i in range(len(nums)):
+    for j in range(i + 1, len(nums)): # 바로 이렇게 따로 first에 1을 주고 계산할 것이 아니라 그냥 i+1 ~ len(a)해주면 되는 것이다!!!
+        if nums[i] + nums[j] == target:
+            return [i, j]
+        
+이렇게 짜면된다
+
+'''
