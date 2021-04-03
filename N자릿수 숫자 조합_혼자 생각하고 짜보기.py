774f132b232348773ebd13de9a888b4ec88ee9e6
Ratatou2 <Ratatou2@users.noreply.github.com>
2021-04-03 13:13:47 +0900

    오류 수정 완료

--- N자릿수 숫자 조합_혼자 생각하고 짜보기.py
+++ N자릿수 숫자 조합_혼자 생각하고 짜보기.py
@@ -1,19 +1,18 @@
 def comb(arr, spot):
     arr = sorted(arr)
-    ans = []
-    def recur(list_to_fill):
-        if len(list_to_fill) == spot:
-            print(list_to_fill)
+
+    def recur(llist):
+
+        if len(llist) == spot:
+            print(llist)
             return
 
+        start = llist[-1] + 1 if llist else 0
 
-        start = list_to_fill[-1] + 1 if list_to_fill else 0
-        # 이 코드가 작동을 안했던 이유는 len을 list_to_fill로 줘서... 당연히 채워진게 없으니 for문이 돌지 않는다
-        for i in range(start, len(list_to_fill)):
-            list_to_fill.append(arr[i])
-            recur(list_to_fill)
-            list_to_fill.pop()
-
+        for i in range(start, len(arr)):
+            llist.append(arr[i])
+            recur(llist)
+            llist.pop()
     recur([])
 
 r = 2
