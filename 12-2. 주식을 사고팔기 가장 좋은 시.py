745d80bfa69acb6203bbee33b98257bb3c4b4e1f
Ratatou2 <ws4232@naver.com>
2021-04-13 18:57:14 +0900

    공부 필요(일단 코드만)

---
+++ 12-2. 주식을 사고팔기 가장 좋은 시.py
@@ -0,0 +1,14 @@
+# 해당 코드는 박상길 저자의 '파이썬 알고리즘 인터뷰'를 공부하며 적은 내용입니다
+# 브루트 포스 방식
+
+def maxProfit(prices:list):
+    max_price = 0
+
+    for i, price in enumerate(prices):
+        for j in range(i, len(prices)):
+            max_price = max(prices[j] - price, max_price)
+
+    return max_price
+
+timing = [7, 1, 5, 3, 6, 4]
+print(maxProfit(timing))
---
+++ 12-3. 주식을 사고팔기 가장 좋은 시.py
@@ -0,0 +1,14 @@
+# 해당 코드는 박상길 저자의 '파이썬 알고리즘 인터뷰'를 공부하며 적은 내용입니다
+# 저점과 현재 값과의 차이 계산
+
+import sys
+
+def maxProfit(prices:list):
+    profit = 0
+    min_price = sys.maxsize
+
+    for price in prices:
+        min_price = min(min_price, price)
+        profit = max(profit, price - min_price)
+
+    return profit
