632982cf9dd54bf3f891f0d6ac94b43e756b0517
Ratatou2 <ws4232@naver.com>
2021-04-15 15:42:10 +0900

    공부 및 해석

--- 13-1. 팰린드롬 연결 리스트.py
+++ 13-1. 팰린드롬 연결 리스트.py
@@ -1,5 +1,9 @@
 # 해당 코드는 박상길 저자의 '파이썬 알고리즘 인터뷰'를 공부하며 적은 내용입니다
 # 리스트 변환
+# 팰린드롬 여부 확인을 위해선 앞뒤로 모두 추출할 수 이는 자료구조가 필요
+# 연결 리스트를 파이썬의 리스트로 변환하고 풀이
+# 동적 배열로 구성된 리스트는 맨 앞 아이템을 가져오기에 적합한 자료형이 아니다
+# 왜냐면 첫번째 값을 꺼내오면 모든 값이 한칸씩 시프팅(shifting)되며, 시간 복잡도 O(n)이 발생하기 때문이다.
 
 def isPalindrome(head):
     q = []
@@ -9,12 +13,19 @@ def isPalindrome(head):
 
     node = head
 
+    # leetcode의 문제라서 val과 next는 따로 지정을 안해줬기에 일반 IDE에선 이 코드 그대로는 돌아가지 않는다
     while node is not None:
         q.append(node.val)
         node = node.next
 
+    # 첫번째 것과 맨 끝의 것의 pop해서 비교해보는데 둘이 일치하지 않는게 있다면 false를 return
     while len(q) > 1:
         if q.pop(0) != q.pop():
             return False
 
     return True
+
+#test = [1, 2]
+test = [1,2,2,1]
+
+print(isPalindrome(test))
--- 13-2. 팰린드롬 연결 리스트.py
+++ 13-2. 팰린드롬 연결 리스트.py
@@ -1,10 +1,14 @@
 # 해당 코드는 박상길 저자의 '파이썬 알고리즘 인터뷰'를 공부하며 적은 내용입니다
 # 데크를 이용한 최적화
+# 데크 = 양방향 큐 / 따라서 양 끝 엘리먼트의 append와 pop이 압도적으로 빠름
+
 import collections
 
 
 def isPalindrome(head):
-    q = collections.deque()
+    q = collections.deque() # import 한 데크를 사용하는 방법
+
+    # 이 외엔 나머지는 거의 다 똑같다
 
     if not head:
         return True
@@ -14,8 +18,15 @@ def isPalindrome(head):
         q.append(node.val)
         node = node.next
 
+    # 양방향 큐의 데크의 장점을 잘 살리는 코드 pop을 left에서 한 것과 맨끝에서 한 것을 비교한다
     while len(q) > 1:
         if q.popleft() != q.pop():
             return False
 
     return True
+
+
+#test = [1, 2]
+test = [1,2,2,1]
+
+print(isPalindrome(test))
