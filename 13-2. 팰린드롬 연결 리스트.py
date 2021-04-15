bdec0ff1e18750c24f87a685e7e63aaddde79ca7
Ratatou2 <ws4232@naver.com>
2021-04-15 13:38:58 +0900

    일단 코드만

--- 13-2. 팰린드롬 연결 리스트.py
+++ 13-2. 팰린드롬 연결 리스트.py
@@ -1,20 +1,21 @@
 # 해당 코드는 박상길 저자의 '파이썬 알고리즘 인터뷰'를 공부하며 적은 내용입니다
-# 리스트 변환
+# 데크를 이용한 최적화
+import collections
+
 
 def isPalindrome(head):
-    q = []
+    q = collections.deque()
 
     if not head:
         return True
 
     node = head
-
     while node is not None:
         q.append(node.val)
         node = node.next
 
     while len(q) > 1:
-        if q.pop(0) != q.pop():
+        if q.popleft() != q.pop():
             return False
 
     return True
