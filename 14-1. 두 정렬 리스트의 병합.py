0ee539c6163349b0dd13f5536b17383b31963cb5
Ratatou2 <ws4232@naver.com>
2021-04-23 15:34:36 +0900

    추가 공부 필요

--- 14-1. 두 정렬 리스트의 병합.py
+++ 14-1. 두 정렬 리스트의 병합.py
@@ -1,9 +1,15 @@
+# 어려워 추가 공부 필요
 
 def mergeTwoLists(l1, l2):
     if (not l1) or (l2 and l1.val > l2.val):
         l1, l2 = l2, l1
 
     if l1:
-        l1.next = self.mergeTwoLists(l1.next, l2)
+        l1.next = mergeTwoLists(l1.next, l2)
 
     return l1
+
+
+
+
+print(mergeTwoLists([1,2,4], [1,3,4]))
