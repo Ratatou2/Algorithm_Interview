ee0e5c5af3199e91821dadc2cd2e8e7e55c84f7d
Ratatou2 <ws4232@naver.com>
2021-04-29 13:19:34 +0900

    코드 및 공부

---
+++ 24. 스택을 이용한 큐 구현.py
@@ -0,0 +1,25 @@
+# 큐를 스택으로 구현하는 방법
+
+class MyQueue:
+    def __init__(self):
+        # 스택 2개 사용
+        self.input = []
+        self.output = []
+
+    def push(self, x):
+        self.input.append(x)
+
+    def pop(self):
+        self.peek()
+        return self.output.pop()
+
+    def peek(self):
+        # output이 없으면 모두 재입력
+        if not self.output:
+            while self.input:
+                self.output.append(self.input.pop())
+
+        return self.output[-1]
+
+    def empty(self):
+        return self.input == [] and self.output == []
