83888483369f9f9ac1aa61bf5af6f22a81016ef7
Ratatou2 <ws4232@naver.com>
2021-05-03 15:58:34 +0900

    코드 공부 및 해석

--- 13-1. 팰린드롬 연결 리스트.py
+++ 13-1. 팰린드롬 연결 리스트.py
@@ -13,17 +13,28 @@ class ListNode:
 class Solution:
     def isPalindrome(self, head: ListNode):
         q = []
+
+        # head(리스트 노드)에 아무것도 없으면 그냥 True 출력
         if not head:
             return True
-
+        
+        # node는 head(리스트노드)이다
         node = head
 
         # leetcode의 문제라서 val과 next는 따로 지정을 안해줬기에 일반 IDE에선 이 코드 그대로는 돌아가지 않는다
+        
+        # node가 not None이면(= node(=head:리스트노드)에 뭔가가 들어있는 한 반복할 문장
+        # 리스트노드에 있는 걸 리스트 q에 추가할거임
+        # 그러고 나서 node는 그 다음노드(node.next)로 이동시킴
+        # 리스트 노드 끝까지 반복해서 리스트 q에 가득 채움
         while node is not None:
             q.append(node.val)
             node = node.next
 
         # 첫번째 것과 맨 끝의 것의 pop해서 비교해보는데 둘이 일치하지 않는게 있다면 false를 return
+        # q 길이가 2 이상일떄까지 반복
+        # 리스트 q의 첫번쨰 요소가 맨 마지막 요소와 같지 않으면 바로 False로 리턴(그게 아니라면 계속 반복)
+        # -> 계속 반복될시 그것은 팰린드롬이 성립하니까 True로 리턴
         while len(q) > 1:
             if q.pop(0) != q.pop():
                 return False
