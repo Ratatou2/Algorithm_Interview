dc46ad3c7ccebd4a79bccd8e5e5ce8ac2f501b6c
Ratatou2 <ws4232@naver.com>
2021-05-03 16:19:07 +0900

    코드 공부 및 해석

--- 13-2. 팰린드롬 연결 리스트.py
+++ 13-2. 팰린드롬 연결 리스트.py
@@ -1,9 +1,16 @@
-# 해당 코드는 박상길 저자의 '파이썬 알고리즘 인터뷰'를 공부하며 적은 내용입니다
-# 데크를 이용한 최적화
-# 데크 = 양방향 큐 / 따라서 양 끝 엘리먼트의 append와 pop이 압도적으로 빠름
+# pop은 첫번째 값을 꺼내오면 하나씩 시프팅되어(맨 앞자리가 비워졌으니 채우려고), 하나씩 옮겨오느라 시간 복잡도 O(n)이 발생한다
+# 이를 해결하기 위해 데크를 쓰는 것
+# 양방향 모두 추출하는데 시간복잡도 O(1)에 실행됨 -> 굉장히 효율적이다
+
+# 따라서 대부분의 코드는 13-1의 리스트와 같다.
+# pop대신 popleft를 쓰는데 popleft는 왼쪽 끝 엘리먼트를 가져오는 동시에 데크에서 삭제한다
 
 import collections
 
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 def isPalindrome(head):
     q = collections.deque() # import 한 데크를 사용하는 방법
