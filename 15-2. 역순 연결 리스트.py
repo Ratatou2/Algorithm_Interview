581a278d72fec3d845eb870cfad926f8ce8b7c3e
Ratatou2 <ws4232@naver.com>
2021-05-04 18:40:12 +0900

    코드 공부 및 해석

--- 15-2. 역순 연결 리스트.py
+++ 15-2. 역순 연결 리스트.py
@@ -10,3 +10,34 @@ def reverseList(head):
         prev, node = node, next
 
     return prev
+
+'''
+실행순서
+
+실행 순서를 일일이 하나 적어보겠다
+먼저 [1-2-3-4-5] 라는 연결리스트가 있다고 할때 맨처음 node는 1이 들어간다
+
+<node = 1>
+next = 2, node.next = None
+prev = 1, node = 2
+
+<node = 2>
+next = 3, node.next = 1
+prev = 2, node = 3
+
+<node = 3>
+next = 4, node.next = 2
+prev = 3, node = 4
+
+<node = 4>
+next = 5, node.next = 2
+prev = 4, node = 5
+
+<node = 5>
+next = None, node.next = 4
+prev = 5, node = None
+
+<node = None>
+return prev 
+prev : 5-4-3-2-1 생성
+'''
