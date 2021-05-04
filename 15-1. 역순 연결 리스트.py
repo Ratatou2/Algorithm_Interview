3bfee62a9468173cd84a26c0ab897215d4363f12
Ratatou2 <ws4232@naver.com>
2021-05-04 18:01:40 +0900

    코드 공부 및 해석_정확히 맞는건지는 헷갈림

--- 15-1. 역순 연결 리스트.py
+++ 15-1. 역순 연결 리스트.py
@@ -15,3 +15,27 @@ head = [1,2,3,4,5]
 #Output: [5,4,3,2,1]
 
 print(reverseList(head))
+
+'''
+실행순서_ 이게 정확히 들어 맞는지는 잘 모르겠다...
+
+실행 순서를 적어보자면
+
+node = 1, prev = None 으로 시작
+next = 2 / prev = 2
+return reverse(2,1)_재귀
+
+next = 3 / prev = 3
+return reverse(3,2)_재귀
+
+next = 4 / prev = 4
+return reverse(4,3)_재귀
+
+next = 5 / prev = 4
+return reverse(5,4)_재귀
+
+next = None / prev = None
+return prev 시작
+
+역순으로 정렬
+'''
