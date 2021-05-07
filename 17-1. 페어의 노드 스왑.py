ee107b6d36ea1140640848502466545b43214d44
Ratatou2 <ws4232@naver.com>
2021-05-07 19:41:38 +0900

    코드 공부 및 해석

--- 17-1. 페어의 노드 스왑.py
+++ 17-1. 페어의 노드 스왑.py
@@ -5,6 +5,8 @@
 def swapPairs(head):
     cur = head
 
+    # while 아래 첫 줄에서 첫번째 값과, 두번째 값이 서로 자리를 바꿈
+    # 그 다음엔 cur가 2개를 건너뜀(해당 값은 2개씩 페어로 묶으니까)
     while cur and cur.next:
         cur.val, cur.next.val = cur.next.val, cur.val
         cur = cur.next.next
