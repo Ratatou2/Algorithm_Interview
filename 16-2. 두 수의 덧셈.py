824c6f3304f832cef56eb1b654c418a417f090bb
Ratatou2 <ws4232@naver.com>
2021-05-07 19:36:36 +0900

    코드 공부 및 이해

--- 16-2. 두 수의 덧셈.py
+++ 16-2. 두 수의 덧셈.py
@@ -1,10 +1,18 @@
-# 전가산기 구현
+# 전가산기 구현 / 논리 회로 원리
+# carry는 자리 올림수 
+# divmod는 파이썬의 내장함수로, 몫과 나머지로 구성된 튜플을 리턴함
+# 뭔소린지 어렵다.. 애초에 이건 그냥 논리 회로로 돌아가는 구성, pass
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 def addTwoNumbers(l1, l2):
     root = head = ListNode(0)
 
     carry = 0
-    while l1 or l2 carry:
+    while l1 or l2 or carry:
         sum = 0
 
         if l1:
