7c2c6681c2e3337c23e92e7d951b4ff5cedd1f74
Ratatou2 <ws4232@naver.com>
2021-05-20 23:28:29 +0900

    코드 공부 및 해석(+ 추가 공부 필요)

--- 19. 역순 연결 리스트2.py
+++ 19. 역순 연결 리스트2.py
@@ -1,5 +1,9 @@
 # 반복 구조로 노드 뒤집기
 
+def ListNode(val=0, next=None):
+    val = val
+    next = next
+
 def reverseBetween(head, m, n):
     if not head or m == n:
         return head
@@ -12,6 +16,9 @@ def reverseBetween(head, m, n):
 
     end = start.next
 
+    # 이 부분이 핵심인데 tmp라고 임시를 만들어줘서 순서를 뒤집음
+    # tmp는 두번째에 고정, 하나씩 끌고오는 셈
+    # 설명이 어렵긴한데 이건 그림을 봐야 이해가 쉬움
     for _ in range(n - m):
         tmp, start.next, end.next = start.next, end.next, end.next.next
         start.next.next = tmp
